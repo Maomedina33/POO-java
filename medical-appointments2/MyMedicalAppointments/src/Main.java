001cfd02f9b42c97267e1e595cb087c990a1def6
Maomedina33 <ingmaomedina@gmail.com>
2025-07-03 18:16:51 -0500

    Add files via upload

---
+++ medical-appointments2/MyMedicalAppointments/src/Main.java
@@ -0,0 +1,20 @@
+import static ui.UIMenu.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Doctor myDoctor = new Doctor();
+        myDoctor.name = "Alejandro Rodríguez";
+        myDoctor.showName();
+        myDoctor.showId();
+
+       
+        Doctor myDoctorAnn = new Doctor();
+        myDoctorAnn.showId();
+
+        showMenu();
+         
+    }
+
+
+}
